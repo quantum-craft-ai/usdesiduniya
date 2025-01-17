cfeb18ede4b0e102e04cbe78090bcbc27d160050
Rajeev <quantumcraft.ai@gmail.com>
2025-01-16 22:18:55 -0500

    Add button

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,14 +2,54 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { StyleClassModule } from 'primeng/styleclass';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { MenuItem, MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, StyleClassModule],
+  imports: [RouterOutlet, FooterComponent, StyleClassModule, SpeedDialModule, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  encapsulation: ViewEncapsulation.None 
+  encapsulation: ViewEncapsulation.None,
+  providers: [MessageService]
 })
 export class AppComponent {
   title = 'frontend';
+  items!: MenuItem[];
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit() {
+    this.items = [
+        {
+            icon: 'pi pi-pencil',
+            command: () => {
+                this.messageService.add({ severity: 'info', summary: 'Add', detail: 'Data Added' });
+            }
+        },
+        {
+            icon: 'pi pi-refresh',
+            command: () => {
+                this.messageService.add({ severity: 'success', summary: 'Update', detail: 'Data Updated' });
+            }
+        },
+        {
+            icon: 'pi pi-trash',
+            command: () => {
+                this.messageService.add({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
+            }
+        },
+        {
+            icon: 'pi pi-upload',
+            routerLink: ['/fileupload']
+        },
+        {
+            icon: 'pi pi-external-link',
+            target: '_blank',
+            url: 'http://angular.io'
+        }
+    ];
+  }
+
 }
--- frontend/src/app/app.config.server.ts
+++ frontend/src/app/app.config.server.ts
@@ -3,11 +3,20 @@ import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRoutesConfig } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRoutesConfig(serverRoutes)
+    provideServerRoutesConfig(serverRoutes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+          preset: Aura
+      }
+  })
   ]
 };
 
